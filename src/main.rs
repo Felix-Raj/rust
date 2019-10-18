ff8adc3f06bad4cb737ba34406d945d7d4c7f160
Felix-Raj <mailtofelixraj@gmail.com>
2019-10-18 17:06:35 +0530

    references-

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,84 @@
 fn main() {
-    let x = if true {
-        // y is known only inside this if -- scope of y
-        let y = 10;
-        y+10
-    } else {
-        0
-    };
-    println!("{}",x);
-
-    // ownership
-   {
-       let mut s = String::from("Hello");  // variable gets a
-       // space in heap rather than on stack as it can grow/shrink.
-       s.push_str(", World!");
-       println!("{}",s)
-   }  // rust calls the drop function for the variable at this point, the
-    // function implements how to return the memory back to OS.
-
-    let x = 5;
-    let y = x;
-    // bind 5 to x, make a copy of x and bind to y
-    // the data is stored to stack rather than heap as the size is known and
-    // does not change
-
-    let mut x = 10;
-    let mut y = x;
-    println!("x and y {} {}", x, y);
-
-    y += 1;
-    println!("x and y {} {} after changing y", x, y);
-    // x is not affected
-
     let s1 = String::from("hello");
-    let s2 = s1;
-    /* string consist of three parts ( in Rust ) -
-        1. pointer to the data (the word "hello") in heap
-        2. length os string
-        3. capacity
-       On assigning s2 with s1, this data is copied, so the pointer for
-       both the variables point to the same location. Rust does not copy the
-       heap data.
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("Hello");
+    change_mut(&mut s);
+    // cannot call change_mut(&mut s1) as s1 is immutable.
+    // Variable should be mutable, reference should be mutable.
+    println!("Mutable {}", s);
+
+    let e1 = &mut s;
+    let e2 = &mut s;
+    // can have only one mutable reference to a particular piece of data in
+    // a particular scope, the following will fail
+    // println!("e1 {} e2 {}", e1, e2);
+    let e1 = &mut s;
+    {
+        let e2 = &mut s;
+        // Does not work
+        // println!("e1 {} e2 {}", e1, e2);
+    }  // e2 moves out of scope here
+    // so
+    let e2 = &mut s;  // is fine
+
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    let r3 = &mut s; // BIG PROBLEM
+    // cannot have a mutable reference when we have immutable references, print
+    // will not work
+    // println!("{}, {}, and {}", r1, r2, r3);
+
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    println!("{} and {}", r1, r2);  //no probs
+    // r1 and r2 are no longer used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+
+    // Dangling References
+    // let ref_to_nothing = dangle();
+
+    /*
+
+    At any given time, you can have either one mutable reference or
+        any number of immutable references.
+    References must always be valid.
+
     */
-    // when both goes out of scope both will try to free
-    // memory ( which is a bug ), so Rust instead of copying the memory it
-    // considers s1 to be invalid. try
 
-    //println!("s1 {}",s1);
-
-    // this is like a shallow copy, but as the variable is invalidated, it is
-    // called *move*.
-
-    // Rust never created *deep copy* by default.
-    let s1 = String::from("Hello");
-    let s2 = s1.clone();  // this copies heap data too.
-    println!("s1 {} s2 {}", s1, s2);
-
-    let x = 5;
-    let y = x;
-    // here there is no change in deep and shallow copy, so both are valid
-    // after the copy
-
-    // if the variable have a Copy trait then the variable is still usable after
-    // assignment. We cannot annotate a type with Copy if the type or any
-    // part of it have implemented the Drop trait.
-
-    // move - copy applies to function arguments and returning as well
-    let s1=gives_ownership();
-    // the return value is *moved* to s1
-    let s2=String::from("Hello");
-    let s3=takes_and_gives_back(s2);
-    // s2 is moved to fn argument a_string, return is moved to s3
-    // as s2 is moved the following throws error
-    // println!("s2 {}",s2);
-    let x = 10;
-    makes_copy(x);  // x is only copied
-    println!("x {} valid after being used to call fn", x);
 }
 
-fn gives_ownership() -> String {
-    let some_string = String::from("Hello");
-    some_string  // Note - no `;`
+fn calculate_length(s: &String) -> usize {
+    // refer the similar fun in functions
+
+    // s gets reference to the string being passed ( that contain the pointer
+    // to the mmr location containing the data, len of str, capacity of str).
+    s.len()
+}  // s goes out of scope, but memory is not freed as s does not have
+// ownership over the parameter, it have just a reference.
+
+fn change(some_string: &String) {
+    // as some_string is *borrowed*, we cannot modify it.
+    // some_string.push_str(", World");
 }
 
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+fn change_mut(some_string: &mut String) {
+    // here the reference is mutable as stated in the argument.
+    some_string.push_str(", World");
 }
 
-fn makes_copy(nu: i32) {
-    println!("{}", nu);
-}
+fn dangle() -> &String {
+    let s = String::from("Hello");
+    &s  // should return the string instead
+}  // s goes out of scope so calling guy gets a pointer to
+// invalid/non-existent data, this is prevented by the compiler
