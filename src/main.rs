adec9bd2f38a23b36121761ef00fd362d8a10acc
Felix-Raj <mailtofelixraj@gmail.com>
2019-10-18 12:36:02 +0530

    something!!

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
-const MAX:u32 = 1000;
-// constants - use const instead of let.
-// should specify the type.
-// can be declared in any scope.
-// can only be set to a constance expression, not to result of function call or any other value
-// that can be computed at run time.
 fn main() {
-    let x=5;
-    println!("Value of x is {}", x);
-    // x = 6;  // x in immutable, cannot reassign
+    // scalar and compound type
 
-    let mut y = 56; // mutable can be changed
-    println!("Value of y is {}", y);
-    y = 12;
-    println!("Value of y is {}", y);
+    // scalar - single value, int, float, bool, char
 
-    // immutable can be shadowed
-    let x = x+MAX;
-    // can change the type when shadowing [see master-guessing game]
-    // as using let, it creates another variable
-    println!("[Shadowing] The value of x is {}", x);
-    // mutable variable cannot be shadowed
-    // y = 100;  // fine, with warnings
-    // y = "felix";  // mismatched types error
+    // also see https://doc.rust-lang.org/book/ch03-02-data-types.html#integer-overflow
+
+    let a=2.3;  // f64
+    let b: f32 = 2.3;  // f32
+    let c = 'x'; // char
+
+    // compound types - multiple values into one type
+    // tuples and arrays
+    let tup: (i32, f64, u8) = (500, 4.5, 12);
+    println!("first element in the tuple is {}", tup.0);
+    let (d, e, f) = tup;  // destructuring
+    println!("e is {}", e);
+
+    let g = [1,2,3,4,5, 4];  // in stack
+    // fixed number of elements
+    // vector type can grow or shrink
+    // homogeneous, [1,2,3,4,5, 4.6, "Felix", 'f'] not allowed
+    // with annotation - g: [i32;6] = [1,2,3,4,5,4];
+    println!("array element 0 {}", g[0]);
+    let h = [3; 4];  // 3 repeated 4 times
 }
