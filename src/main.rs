c451fbeb1efdc06c52c5790b357a969380e8d532
Felix-Raj <mailtofelixraj@gmail.com>
2019-10-18 14:51:00 +0530

    control--flow!!!

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,94 @@
 fn main() {
-    println!("Hello, World!");
+    let number = 3;
 
-    // expression - evaluates to a value
-    // statements - do not return a value
-    // let x=4 is an expression, while 4 is a statement. Statement 4 returns
-    // 4, while let x=4 returns nothing. Thus we cannot do let y = (let x = 4)
-    let y = {
-        let x= another_function(4);
-        x + 1
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    // condition *must be* a bool
+    /*if number {
+        // wont compile
+    }*/
+
+    let number = if true {
+        number + 2  // no semi-colon!!
+    } else {
+        number
+        // "six" // <-- Error!!!, all arms should be of same type
     };
-    // try with let y = { let x = 3; x+1;}
-    // with the ending `;` this turns to be a statement which cannot be
-    // assigned to a variable
-    println!("y is {}", y);
+    // when using if to assign value to an variable, each arm should evaluate
+    // to compatible data type
+
+    println!("number is {}", number);
+
+    let mut ctr = 0;
+    loop {
+        println!("Counter value {}", ctr);
+        ctr += 1;
+        if ctr >= 4 {
+            break;
+        }
+    }
+
+    ctr = 0;
+    // do operations that may fail repeatedly
+    let result = loop {
+        ctr += 1;
+        if ctr == 10 {
+            break ctr * 2;
+            // or break ctr * 2
+        }
+    };
+    println!("Result {}", result);
+
+    ctr = 0;
+    while ctr != 4 {
+        println!("ctr {}", ctr);
+        ctr += 1;
+    };
+    // assignment to variable as in loop does not work here, also break does
+    // not work here. Should be like
+    ctr = 0;
+    while ctr != 4 {
+        println!("ctr wb {}", ctr);
+        ctr += 1;
+        if ctr == 2{
+            // break ctr; //  <- does not work
+            break;
+        }
+    };
+
+    let a:[i32;4] = [10, 20, 12, 34];
+    for x in a.iter() {
+        println!("The value is {}",x);
+    };
+
+    // range
+    for y in 1..10 {
+        println!("The value of y {}", y);
+        println!("fib({}) = {}", y, fib(y));
+    }
 }
 
-fn another_function(x: i32) -> i32 {
-    println!("Value if x is {}", x);
-    return x + 1;
-    // x + 1 is also valid, notice the missing `;`, as this should be an
-    // expression rather than a statement;
+fn fib(pos: i32) -> i32 {
+    let mut a: i32 = 1;
+    let mut b: i32 = 1;
+    let mut c: i32 = 0;
+    let mut pos = pos;
+    if pos == 1 {
+        return a;
+    }
+    while pos > 1 {
+        c = a;
+        a = b;
+        b = c + b;
+        pos -= 1;
+    }
+    return a;
 }
