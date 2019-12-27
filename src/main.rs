21246841c7731cf4e9c2cdd2a8d7c3ad34993b37
Felix-Raj <mailtofelixraj@gmail.com>
2019-12-27 22:22:56 +0530

    reading the file content

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::env;
+use std::fs::read_to_string;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    // println!("{:?}", args);
+    let filename = &args[1];
+    let search_for = &args[2];
+
+    println!("In file {}", filename);
+
+    let contents = read_to_string(filename)
+        .expect("Something went wrong on opening the file");
+    println!("Content of the file:\n{}",contents);
 }
