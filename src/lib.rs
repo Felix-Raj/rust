7008b88aaf4eb159e2c1d022692b046486b9a679
Felix-Raj <mailtofelixraj@gmail.com>
2019-12-30 20:42:13 +0530

    panic on not enough arguments

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ pub struct Config {
 
 impl Config {
     pub fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments")
+        }
         Config {
             filename: args[1].clone(),
             query: args[2].clone(),
